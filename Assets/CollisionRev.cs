061f783963478ae35b4935ca049b443507bc4539
pmlpml <pmlpml@163.com>
2018-04-18 11:46:52 +0800

    physics

---
+++ Assets/CollisionRev.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionRev : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision) {
+		Debug.Log ("Collision @ " + Time.frameCount.ToString());
+		Debug.Log ("GameObject is :" + collision.gameObject.name);
+		if (collision.collider) Debug.Log ("Collider belong to :" + collision.collider.gameObject.name);
+		if (collision.rigidbody) Debug.Log ("Rigidbody belong to :" + collision.rigidbody.gameObject.name);
+		foreach (ContactPoint contact in collision.contacts) {
+			Debug.DrawRay (contact.point, contact.normal, Color.white);
+		}
+	}
+}
---
+++ Assets/Gravity.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+
+	void FixedUpdate () {
+		Rigidbody rigid = this.gameObject.GetComponent<Rigidbody> ();
+		if (rigid) {
+			rigid.AddForce (Vector3.down * 9.8f);
+		}
+	}
+}
